10818e4ebbe2c29e005b628578a0c8320c813ec2
netospadoni <netospadoni@gmail.com>
2025-02-06 01:25:04 -0400

    docs: add Swagger documentation to FederacaoController

--- src/main/java/com/gestaoesportiva/api/controller/FederacaoController.java
+++ src/main/java/com/gestaoesportiva/api/controller/FederacaoController.java
@@ -5,6 +5,8 @@ import com.gestaoesportiva.api.domain.federacao.DadosAtualizacaoFederacao;
 import com.gestaoesportiva.api.domain.federacao.DadosCadastroFederacao;
 import com.gestaoesportiva.api.domain.federacao.DadosListagemFederacao;
 import com.gestaoesportiva.api.service.FederacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,12 +18,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/federacoes")
+@Tag(name = "Federações", description = "Gerenciamento de Federações")
 public class FederacaoController {
 
     @Autowired
     private FederacaoService federacaoService;
 
     @GetMapping
+    @Operation(summary = "Lista todas as Federações", description = "Retorna uma página com todas as federações")
     public ResponseEntity<Page<DadosListagemFederacao>> listarUsuario(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         var pagina = federacaoService.listarFederacao(paginacao);
         return ResponseEntity.ok(pagina);
@@ -29,6 +33,7 @@ public class FederacaoController {
 
     @PostMapping
     @Transactional
+    @Operation(summary = "Adiciona uma nova Federação", description = "Cadastra uma nova Federação na lista")
     public ResponseEntity<Federacao> criarFederacao(@RequestBody DadosCadastroFederacao dados) {
         try {
             Federacao novaFederacao = federacaoService.criarFederacao(dados);
@@ -40,6 +45,7 @@ public class FederacaoController {
 
     @PutMapping
     @Transactional
+    @Operation(summary = "Atualizar informações da Federação", description = "Atualiza as informações de uma Federação")
     public ResponseEntity<Federacao> atualizarInformacoesFederacao(@RequestBody DadosAtualizacaoFederacao dados) {
         Federacao novaFederacao = federacaoService.atualizarFederacao(dados);
         return ResponseEntity.ok(novaFederacao);
@@ -47,6 +53,7 @@ public class FederacaoController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @Operation(summary = "Deletar Federação", description = "Retorna 'ativo' = false, tornando uma federação inativa")
     public ResponseEntity<?> inativarFederacao(Long id) {
         try {
             federacaoService.inativarFederacao(id);
@@ -58,6 +65,7 @@ public class FederacaoController {
 
     @PutMapping("/ativar")
     @Transactional
+    @Operation(summary = "Ativar Federação", description = "Retorna 'ativo' = true, tornando uma federação ativa novamente")
     public ResponseEntity<?> ativarFederacao(Long id) {
         try {
             federacaoService.ativarFederacao(id);
